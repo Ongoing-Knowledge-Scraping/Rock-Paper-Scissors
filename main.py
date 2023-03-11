9d36faf8933e1c1d57c27eedb626f9bdbb78e809
KP Patzer <126931254+kpatzer@users.noreply.github.com>
2023-03-11 00:56:55 +0000

    Moved invalid number section to the top creating opening if / else

--- main.py
+++ main.py
@@ -32,22 +32,23 @@ gameImages = [rock, paper, scissors]
 
 userChoice = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors. \n"))
 
-print(gameImages[userChoice])
-
-computerChoice = random.randint(0, 2)
-print("Computer chose:")
-print(gameImages[computerChoice])
-
 if userChoice >= 3 or userChoice < 0:
   print("You typed an invalid number, you lose!")
-elif userChoice == 0 and computerChoice == 2:
-  print("You win!")
-elif computerChoice == 0 and userChoice == 2:
-  print("You lose!")
-elif computerChoice > userChoice:
-  print("You lose!")
-elif userChoice > computerChoice:
-  print("You win!")
-elif computerChoice == userChoice:
-  print("It\'s a draw")
+else:
+  print(gameImages[userChoice])
+
+  computerChoice = random.randint(0, 2)
+  print("Computer chose:")
+  print(gameImages[computerChoice])
+
+  if userChoice == 0 and computerChoice == 2:
+    print("You win!")
+  elif computerChoice == 0 and userChoice == 2:
+    print("You lose!")
+  elif computerChoice > userChoice:
+    print("You lose!")
+  elif userChoice > computerChoice:
+    print("You win!")
+  elif computerChoice == userChoice:
+    print("It\'s a draw")
   
